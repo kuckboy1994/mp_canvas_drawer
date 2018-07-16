23bf820d50026a7d187326dbe56686c1c8d4f263
kuckboy1994 <shanchao@qq.com>
2018-07-16 14:31:00 +0800

    :art: 增加 `measureText` 方法对于低版本的提示

--- components/canvasdrawer/canvasdrawer.js
+++ components/canvasdrawer/canvasdrawer.js
@@ -94,7 +94,14 @@ Component({
           })
           imageIndex++
         } else if (views[i].type === 'text') {
-          this.drawText(views[i])
+          if (!wx.canIUse('measureText')) {
+            wx.showModal({
+              title: '提示',
+              content: '当前微信版本过低，无法使用 measureText 功能，请升级到最新微信版本后重试。'
+            })
+          } else {
+            this.drawText(views[i])
+          }
         } else if (views[i].type === 'rect') {
           this.drawRect(views[i])
         }
